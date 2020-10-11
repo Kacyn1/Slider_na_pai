845d5a14364800b82c3f8ef238cdd9d3edb2630b
Kacyn1 <62652276+Kacyn1@users.noreply.github.com>
2020-10-11 21:42:36 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("imgs");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+var id=0;
+var zdjecia = document.querySelectorAll('#imgs img');
+var memy = document.getElementById('imgs');
+function slider() {
+    id++;
+    if(id>=zdjecia.length) {
+        id=0;
+    }
+    memy.style.transform=`translateX(${-id*500}px)`;
 }
+setInterval(slider,2000);
+
